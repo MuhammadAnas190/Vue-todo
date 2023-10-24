4dae4e0192a97d4a13f76d756d7c588687844086
Muhammad Anas Saud <mohammad.anas190@gmail.com>
2023-10-24 15:56:16 +0500

    feat: init and create todo app

---
+++ src/components/TodoElement.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+  props: ['todoList', 'addTodo', 'removeTodo', 'completeTodo'],
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    handleChangeTodo(evt) {
+      this.message = evt.target.value
+    },
+    handleAddTodo() {
+      console.log('add', this.message, this.todoList)
+      this.addTodo(this.message)
+      this.message = ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="todo">
+    <div class="todo-element" v-for="(item, index) of this.todoList" :key="item">
+      <h4 :class="item.status === 1 && 'complete'" @click="completeTodo(index)">
+        {{ item.message }}
+      </h4>
+      <div>
+        <button @click="removeTodo(index)">Delete</button>
+      </div>
+    </div>
+  </div>
+  <div class="add-todo">
+    <input type="text" @input="handleChangeTodo" />
+    <button type="text" @click="handleAddTodo">Add</button>
+  </div>
+</template>
+
+<style scoped>
+.todo {
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.todo-element {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+h4 {
+  font-weight: 500;
+  font-size: 1.5rem;
+  position: relative;
+  top: -10px;
+}
+.add-todo {
+  display: flex;
+  justify-content: space-between;
+  & input {
+    width: 80%;
+  }
+}
+.complete {
+  color: #51b4a3;
+  text-decoration: line-through;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<script>
+import TodoElement from '@/components/TodoElement.vue'
+
+export default {
+  data: () => ({
+    todoList: []
+  }),
+  methods: {
+    addTodo(todo) {
+      const todoItem = {
+        message: todo,
+        status: 0 // 0 is inComplete, 1 is Complete
+      }
+      this.todoList = [...this.todoList, todoItem]
+    },
+    removeTodo(index) {
+      this.todoList.splice(index, 1)
+    },
+    complete(index) {
+      this.todoList[index].status = 1
+    }
+  },
+  components: { TodoElement }
+}
+</script>
+
+<template>
+  <main class="todo-wrapper">
+    <TodoElement
+      :todoList="todoList"
+      :addTodo="addTodo"
+      :removeTodo="removeTodo"
+      :completeTodo="complete"
+    />
+  </main>
+</template>
+
+<style scoped>
+.todo-wrapper {
+  width: 80dvh;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 8dvh 0 2dvh;
+}
+</style>
